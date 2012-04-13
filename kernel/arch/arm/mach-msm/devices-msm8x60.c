1849e54ec1ef8050bb6ee739bc53c9c150381290
DooMLoRD <metoo.mememe@gmail.com>
2012-04-14 03:45:30 +0530

    testing GPU overclocking

--- kernel/arch/arm/mach-msm/devices-msm8x60.c
+++ kernel/arch/arm/mach-msm/devices-msm8x60.c
@@ -767,7 +767,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwr_data = {
 		.pwrlevel = {
 			{
-				.gpu_freq = 266667000,
+				.gpu_freq = 320000000,
 				.bus_freq = 4,
 				.io_fraction = 0,
 			},
