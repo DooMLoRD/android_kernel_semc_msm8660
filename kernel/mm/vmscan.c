5619c3a370aef6e320373ac56ebfd8ad0df55961
DooMLoRD <metoo.mememe@gmail.com>
2012-04-23 03:20:56 +0530

    updated defconfig to enable zcache, cleancache, frontswap & some fixes for zcache

--- kernel/mm/vmscan.c
+++ kernel/mm/vmscan.c
@@ -502,6 +502,8 @@ void putback_lru_page(struct page *page)
 	int was_unevictable = PageUnevictable(page);
 
 	VM_BUG_ON(PageLRU(page));
+	if (active)
+		SetPageWasActive(page);
 
 redo:
 	ClearPageUnevictable(page);
@@ -1033,6 +1035,7 @@ static unsigned long clear_active_flags(struct list_head *page_list,
 		if (PageActive(page)) {
 			lru += LRU_ACTIVE;
 			ClearPageActive(page);
+			SetPageWasActive(page);
 			nr_active++;
 		}
 		count[lru]++;
@@ -1369,6 +1372,7 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
+		SetPageWasActive(page);
 		list_add(&page->lru, &l_inactive);
 	}
 
